97b7e89c6882a7755d5f832fa75d7793d4efcc13
unknown <voldam@ lolamgit config --global user.email volidam>
2024-03-19 17:47:27 +0300

    it was my homework

---
+++ main.js
@@ -0,0 +1,28 @@
+// const front951 = [];
+// front951[0] = "Nasiba"
+// front951[1] = "zubayr"
+// front951[2] = "Muhammadjon"
+// front951[3] = "bexruz"
+// alert(front951);
+
+
+
+
+const array = ["Mars", "Yupiter", "Saturin", "Venera"];
+let long = array.length;
+console.log("Arrayning uzunligi  = " + long);
+alert("Arrayning uzunligi  = " + long);
+
+let result = confirm("Siz jarayon davom etishini hohlaysizmi?");
+
+
+if (result) {
+    array.pop();
+    let1 = array.length;
+    alert("Kesilgan arrayning uzunligi  = " + 1);
+    console.log("Kesilgan arrayning uzunligi  = " + 1);
+} else {
+    let1 = array.length;
+    alert("Siz jarayyoni toxtatishingiz uchun  =" + 1);
+    console.log("Siz jarayonni toxtashingiz uchun  =" + 1);
+}     
